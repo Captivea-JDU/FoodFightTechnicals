6f6bfe827e0d2a6313cad963b5ee0d0f9662acb7
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-07-19 11:55:36 -0400

    pulling customer phone number to the associated sale order

---
+++ custom_mod_extended/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Custom Module',
+    'category': 'Sales',
+    'summary': "Pulls phone number associated with sale order",
+    'version': '1.0 (v13)',
+    'author': 'Captivea software Consulting, Jarvis Dumas',
+    'website': 'https://www.captivea.com/',
+    'license': 'OPL-1',
+    'description': """Pulls phone number associated with sale order
+        """,
+    'depends': ['contacts'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ custom_mod_extended/models/custom_mod_extended.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+import logging
+_logger = logging.getLogger(__name__)
+
+class CustomModExtended(models.Model):
+    _inherit = 'sale.order'    
+
+        cust_number = env['res.partner'].phone
+       
+            
+        return {
+            'cust_number': cust_number,
+
+        }
+    
+

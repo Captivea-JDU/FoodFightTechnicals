0297ad189b30b29bffffe57a27c5a7b2e367a3cf
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-07-19 12:07:11 -0400

    Update custom_mod_extended.py

--- custom_mod_extended/models/custom_mod_extended.py
+++ custom_mod_extended/models/custom_mod_extended.py
@@ -5,14 +5,13 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class CustomModExtended(models.Model):
-    _inherit = 'sale.order'    
-
+    _inherit = 'sale.order'
     cust_number = env['res.partner'].phone
-       
-            
+    
     return {
-        'cust_number': cust_number,
+    'cust_number': cust_number,
 
-        }
+
+    }
     
 

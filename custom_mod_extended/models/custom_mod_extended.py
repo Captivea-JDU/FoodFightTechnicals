f3113100f7148949132db908ca988c45139fe1be
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-07-20 14:52:48 -0400

    Update custom_mod_extended.py

--- custom_mod_extended/models/custom_mod_extended.py
+++ custom_mod_extended/models/custom_mod_extended.py
@@ -7,7 +7,17 @@ _logger = logging.getLogger(__name__)
 class CustomModExtended(models.Model):
 
     _inherit = 'sale.order'
-    cust_number = env['res.partner'].phone
+    cust_number = 25
+    actual_num = 23
+    sum_num = 0
+
+    def addone(cust_number):
+        if cust_number = 25:
+            sum_num = actual_num + cust_number
+
+            return sum_num
+
+
 
     
    # return 
